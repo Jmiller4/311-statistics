4cad1915feccb7baf51a3d6e192c10ed80b7e212
Joel Miller <jmiller4@oberlin.edu>
2020-03-31 23:47:45 -0500

    ignore old requests & bad zips, sort table, add output.csv

--- number_crunching.py
+++ number_crunching.py
@@ -1,4 +1,5 @@
 from csv import reader
+from csv import writer
 import datetime
 from tabulate import tabulate
 from statistics import mean
@@ -8,7 +9,9 @@ import os.path
 def main():
 
     '''
-    code to calculate average time between graffiti "registration (311 complaint)" & removal, by zip code, in Chicago.
+    code to calculate average time between the creation of graffitti complaints and the removal of said graffitti,
+     by zip code, in Chicago.
+
     data set can be found at
     https://data.cityofchicago.org/Service-Requests/311-Service-Requests-Graffiti-Removal-Historical/hec5-y4x5/data
     with a download link at
@@ -16,7 +19,16 @@ def main():
 
     Note: this is historical data, thru 2018. Current dataset is at
     https://data.cityofchicago.org/Service-Requests/311-Service-Requests/v6vf-nfxy/data
+
+    one interesting thing is, there are more than a few requests in here which are quite old --
+    graffitti complaints registered in the 20th century (as early as the 1920s!) that only got covered up in the 2010s.
+    To keep things relevant, I have a boolean called "ignore_old_requests" and a variable called "request_year_threshhold".
+    If ignore_old_requests is True, then we only look at requests that were logged after request_year_threshhold.
+
     '''
+
+    ignore_old_requests = True
+    request_year_threshhold = 2005
 
     fname = '311_Service_Requests_-_Graffiti_Removal_-_Historical.csv'
 
@@ -39,13 +51,26 @@ def main():
 
         for row in data_reader:
 
+            # get date request was filed, figure out if it's too old
+            creation_date = datetime.datetime.strptime(row[creation_index], '%m/%d/%Y')
+            if ignore_old_requests and creation_date.year < request_year_threshhold:
+                continue
+
+            # get the zip code
             zipcode = row[zip_index]
+
+            # catch bad zips
+            if len(zipcode) != 5:
+                zipcode = 'Invalid'
+
+            # make a new entry in our dictionary if we need to
             if zipcode not in completion_stats.keys():
                 completion_stats[zipcode] = {'uncompleted': 0, 'differences': []}
 
             if row[status_index] == 'Completed':
 
-                creation_date = datetime.datetime.strptime(row[creation_index] , '%m/%d/%Y')
+                #get the time from the creation of the request to its completion, and update our dictionary
+
                 completion_date = datetime.datetime.strptime(row[completion_index], '%m/%d/%Y')
                 difference = completion_date - creation_date
 
@@ -55,8 +80,16 @@ def main():
                 completion_stats[zipcode]['uncompleted'] += 1
                 other_stats['total_uncompleted'] += 1
 
-        list_for_table = []
+
+        # calculate averages and standard deviations
+
+        final_data = []
         for zipcode in completion_stats.keys():
+
+            # ignore records with invalid zip codes
+            if zipcode == 'Invalid':
+                continue
+
             difference_list = completion_stats[zipcode]['differences']
             avg = mean(difference_list)
             if len(difference_list) > 1:
@@ -66,16 +99,27 @@ def main():
             count = len(difference_list)
             uncompleted = completion_stats[zipcode]['uncompleted']
 
-            list_for_table.append([zipcode, avg, std_deviation, count, uncompleted])
+            final_data.append([zipcode, avg, std_deviation, count, uncompleted])
 
-        nice_table = tabulate(list_for_table, headers=['Zip code', 'avg completion time', 'std dev of completion time',\
-                                                '# of completed requests', '# of unfulfilled requests'])
+        final_data.sort(key=lambda x: x[1])
+        final_data.reverse()
 
+        headers = ['Zip code', 'avg completion time', 'std dev of completion time',\
+                                                '# of completed requests', '# of unfulfilled requests']
+
+        # write output as a nicely formatted table
         f = open('output.txt', 'w')
-        f.write(nice_table)
+        f.write(tabulate(final_data, headers=headers))
+        if ignore_old_requests:
+            f.write('\n\nStats calculated ignoring all requests entered before ' + str(request_year_threshhold))
         f.close()
 
-
+        # write output as csv
+        with open('output.csv', 'w', newline='') as file:
+            csv_writer = writer(file)
+            csv_writer.writerow(headers)
+            for row in final_data:
+                csv_writer.writerow(row)
 
 main()
 
